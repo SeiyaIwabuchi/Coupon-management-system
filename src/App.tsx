9e747b10186af0d781032c67ef03f8563b90e4e8
SeiyaIwabuchi <0123ook@gmail.com>
2021-02-12 17:55:53 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import Common from './Common';
+
+function App() {
+	return (
+		<Box>
+			<Common/>
+		</Box>
+	);
+}
+
+export default App;
---
+++ src/Common.tsx
@@ -0,0 +1,21 @@
+
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { ICommonProps } from "./propInterfaces";
+
+export default function Common(props: ICommonProps) {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                    News
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
