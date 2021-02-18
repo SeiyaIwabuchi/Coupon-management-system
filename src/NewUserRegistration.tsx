66f482dd65763ceadd7d41d39dd8b4e5c21f49d0
SeiyaIwabuchi <0123ook@gmail.com>
2021-02-18 22:38:54 +0900

    新規登録画面の実装

---
+++ src/NewUserRegistration.tsx
@@ -0,0 +1,61 @@
+import { Typography, Box, TextField, Button } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+
+interface INewUserRegistrationProps{
+    setAppbar:{
+        leftIcon:(icon:JSX.Element) => void,
+        centerTitle:(title:string) => void;
+        rightIcon:(icon:JSX.Element) => void;
+    };
+}
+
+
+export default function NewUserRegistration(props:INewUserRegistrationProps) {
+
+
+    let history = useHistory();
+
+
+    const [loginID,sLoginID] = useState("");
+    const [passwd,sPasswd] = useState("");
+
+
+    let loginButtonHandle = () => {
+        localStorage.setItem("uid",loginID);
+        history.push("/coupon");
+    };
+
+
+    useEffect(()=>{
+        props.setAppbar.leftIcon(<></>);
+        props.setAppbar.centerTitle("新規登録");
+        props.setAppbar.rightIcon(<></>);
+    },[]);
+
+
+    return (
+        <Box style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <Box style={{
+                width: "80vw",
+                height: "80vw",
+                display: "flex",
+                flexDirection: "column",
+                border: "1px solid #919191",
+                borderRadius: "10px",
+                padding: "20px",
+                margin: "20px"
+            }}>
+                <div style={{ flexGrow: 1 }}/>
+                <Typography variant="h5" align="center" style={{ flexGrow: 2 }}>
+                    新規登録
+                </Typography>
+                <TextField value={loginID} onChange={event=>{sLoginID(event.target.value)}} variant="filled" placeholder="ログインID" label="ログインID" style={{ flexGrow: 2 }} />
+                <TextField value={passwd} onChange={event=>{sPasswd(event.target.value)}} variant="filled" placeholder="パスワード" label="パスワード" type="password" style={{ flexGrow: 2 }} />
+                <Button variant="contained" color="primary" component={Link} to="login">登録</Button>
+                <div style={{ flexGrow: 1 }}/>
+            </Box>
+        </Box>
+    );
+}
